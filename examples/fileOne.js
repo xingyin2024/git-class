978ff5cda8607a538170f803aab414ec6c0c0759
Diego Zito <diegozito.m@gmail.com>
2024-10-09 15:28:04 +0200

    update - force conflict

--- examples/fileOne.js
+++ examples/fileOne.js
@@ -1,9 +1,1 @@
-console.log("helloooo");
-
-console.log("Fixing this bug over here.....");
-
-console.log("some other changes....");
-
-console.log("more chages on bug fix 01 - Added this to cconflicttt...");
-
-console.log("Added on main ....");
+const name = "Juan";
